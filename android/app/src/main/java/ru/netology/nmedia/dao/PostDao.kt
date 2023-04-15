a2b163c8ff4637cda8bc5e49cb710b175184efc5
ANATOLII MERENKOV <108669729+AnatoliyMerenkov@users.noreply.github.com>
2023-04-16 01:18:55 +0300

    Flow

---
+++ android/app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
@@ -0,0 +1,33 @@
+package ru.netology.nmedia.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import ru.netology.nmedia.entity.PostEntity
+
+@Dao
+interface PostDao {
+    @Query("SELECT * FROM PostEntity ORDER BY id DESC")
+    fun getAll(): Flow<List<PostEntity>>
+
+    @Query("SELECT * FROM PostEntity WHERE show = 0")
+    suspend fun getUnreadPosts()
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(post: PostEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(posts: List<PostEntity>)
+
+    @Query("SELECT COUNT(*) == 0 FROM PostEntity")
+    suspend fun isEmpty(): Boolean
+
+    @Query("DELETE FROM PostEntity WHERE id = :id")
+    suspend fun removeById(id: Long)
+
+    @Query("UPDATE PostEntity SET show = 1")
+    suspend fun readAll()
+
+}
